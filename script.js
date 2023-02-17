a6149cd57d253b02652679234a108cbe3e4a8c05
Jordan <jnwong116@gmail.com>
2023-02-17 00:48:09 -0500

    Set up initial role list and html page

---
+++ script.js
@@ -0,0 +1,24 @@
+const rolesList = [
+  "Godfather",
+  "Mafia",
+  "Blackmailer",
+  "Janitor",
+  "Consort",
+  "Serial Killer",
+  "Werewolf",
+  "Executioner",
+  "Jester",
+  "Veteran",
+  "Sheriff",
+  "Doctor",
+  "Lookout",
+  "Escort",
+  "Transporter",
+  "Bodyguard",
+  "Mayor",
+  "Vigilante",
+];
+
+function startGame() {
+  console.log("Game started");
+}
